72643bbcb3a31986d0d37f8a2c40589ee1af03fb
ravish4596 <42179850+ravish4596@users.noreply.github.com>
2021-09-13 22:07:42 +0530

    Fibonacci series program added

---
+++ fibonnaci.js
@@ -0,0 +1,20 @@
+function fibonacci(n) {
+    /*
+		@parameters:
+		prev: preceding number
+		curr: current number
+		sum: sum of two preceding number
+		output: contains fibonacci series
+	*/
+	let prev = 0, curr = 1, sum, output = [prev, curr];
+	
+	// Loop to get n series
+    for (i = 2; i < n; i++) {
+        sum = prev + curr;
+        prev = curr;
+        curr = sum;
+        output.push(sum);
+    }
+    return output;
+}
+console.log(fibonacci(8));
