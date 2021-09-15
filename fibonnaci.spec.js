b4c17b19daeb6854c9cdab143d282a094d7248af
ravish4596 <42179850+ravish4596@users.noreply.github.com>
2021-09-15 11:45:34 +0530

    Update fibonnaci.spec.js
    Updated test case

--- fibonnaci.spec.js
+++ fibonnaci.spec.js
@@ -5,7 +5,7 @@ describe('Fibonacci Series', () => {
 		this.timeout(1000);
 	});
 	
-	it('should print fibonacci series upto 4', (done) => {
-		expect(fibonnaci(4)).toBe(24);
+	it('should print fibonacci series upto 8', (done) => {
+		expect(fibonnaci(8)).toBe([0, 1, 1, 2, 3, 5, 8, 13]);
 	});
 })
